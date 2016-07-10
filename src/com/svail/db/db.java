cbaa6185f1e155d220aba906e95bdf285880ec13
zhouxiang <723395286@qq.com>
2016-07-10 13:04:54 +0800

    update

--- src/com/svail/db/db.java
+++ src/com/svail/db/db.java
@@ -7,19 +7,24 @@ import com.mongodb.Mongo;
  * Created by ZhouXiang on 2016/7/2.
  */
 public class db {
+    static Mongo m;
+    static DB db;
     public static DB getDB(){
-        Mongo m;
-        DB db;
-        try {
-            System.out.println("运行开始:");
-            m = new Mongo("192.168.6.9", 27017);
-            //m = new Mongo("127.0.0.1", 27017);
-            //m.dropDatabase("test");
-            db= m.getDB("houseprice");
+        if(m==null&&db==null){
+                try {
+                    System.out.println("运行开始:");
+                    m = new Mongo("192.168.6.9", 27017);
+                    //m = new Mongo("127.0.0.1", 27017);
+                    //m.dropDatabase("test");
+                    db= m.getDB("houseprice");
+                    return db;
+                }catch (Exception e){
+                    e.printStackTrace();
+                    return null;
+                }
+        }else{
             return db;
-        }catch (Exception e){
-            e.printStackTrace();
-            return null;
+
         }
     }
 }
