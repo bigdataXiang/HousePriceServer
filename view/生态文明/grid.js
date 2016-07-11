431f4f8c9dba323145d4c2ee84941436983522be
zhouxiang <723395286@qq.com>
2016-07-11 16:06:45 +0800

    能唯一确定网格的曲线啦

---
+++ view/生态文明/grid.js
@@ -0,0 +1,37 @@
+/**
+ * Created by ZhouXiang on 2016/7/11.
+ */
+var grid = L.rectangle.extend({
+
+    initialize: function (latlng,data) {
+        // save position of the layer or any options from the constructor
+        this._latlng = latlng;
+        this.data=data;
+    },
+
+    onAdd: function (map) {
+        this._map = map;
+
+        // create a DOM element and put it into one of the map panes
+        this._el = L.DomUtil.create('div', 'my-custom-layer leaflet-zoom-hide');
+        map.getPanes().overlayPane.appendChild(this._el);
+
+        // add a viewreset event listener for updating layer's position, do the latter
+        map.on('viewreset', this._reset, this);
+        this._reset();
+    },
+
+    onRemove: function (map) {
+        // remove layer's DOM elements and listeners
+        map.getPanes().overlayPane.removeChild(this._el);
+        map.off('viewreset', this._reset, this);
+    },
+
+    _reset: function () {
+        // update layer's position
+        var pos = this._map.latLngToLayerPoint(this._latlng);
+        L.DomUtil.setPosition(this._el, pos);
+    }
+});
+
+map.addLayer(new MyCustomLayer(latlng));
