ae7dc348debac2a7533c306704d4967fcd171dac
zhouxiang <723395286@qq.com>
2016-09-17 23:51:31 +0800

    哎呀数据好烦

--- src/utils/Txt_Excel.java
+++ src/utils/Txt_Excel.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.util.Vector;
 
+import net.sf.json.JSONException;
 import net.sf.json.JSONObject;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
@@ -16,168 +17,302 @@ import org.apache.poi.ss.usermodel.Workbook;
 
 
 public class Txt_Excel {
-	public String TITLE;
-	public double lng = 0.0;
-	public double lat = 0.0;
-	public String DIRECTION = null;
-	public static Object INVALID_DEGREE;
-	public String TIME;
-	public String PRICE;
-	public String HOUSE_TYPE;
-	public String COMMUNITY;
-	public String ADDRESS;
-	public String PARTMENT;
-	public String AREA;
-	public String FLOOR;
-	public String DECORATION;
-	public String TRAFFIC;
-	public String URL;
-	public String date;
-	public String COORDINATE;
-	public String DEPOSIT;
-	public String RENT_TYPE;
-	public String PROPERTY_TYPE;
-	public String LOCATION;
-	public String SOURCE;
-	public String DOWN_PAYMENT;
-	public String UNIT_PRICE;
-	public String ORIENTATION;
-	public String FITMENT;
-	public String GENERAL_SITUATION;
-	public String infloor;
-	public String totalfloor;
-	public int roughcast;
-	public int Simple_decoration;
-	public int Fine_decoration;
-	public int Luxury_decoration;
-	public int Moderate_decoration;
-	public int south;
-	public int north;
-	public int east;
-	public int west;
-	public int south_north;
-	public int east_west;
-	public int west_south;
-	public int east_north;
-	public int west_north;
-	public int east_south;
-	public String DEVELOPER;
-	public String PROPERTY;
-	public String PROPERTY_FEE;
-	public String TOTAL_AREA;
-	public String BUILT_YEAR;
-	public String HOUSEHOLDS;
-	public String VOLUME_RATE;
-	public String PARK;
-	public String GREEN_RATE;
-	public Double LNG;
-	public Double LAT;
-	public String Name;
-	public String PostCoor;
-	public String PostCoorLN;
-	public String PostCoorLA;
-	public String PostReg;
-	public String Code;
-	public String CodeAddr;
-	public String CodeCoor;
-	public String CodeCoorLN;
-	public String CodeCoorLA;
-	public String CodeReg;
-	public String CtfId;
-	public String Home;
-	public String Gender;
-	public String Birth;
-	public String PostAddr;
-	public String Mobile;
-	public static String Folder="D:\\test\\woaiwojia_rentout_2016_0401.txt";
-	public static void main(String argv[]) throws Exception{
-	    System.out.println("测试开始!");
-	    writeExcel();
-	    System.out.println("测试结束!");
-	   
- }
-   public static void writeExcel() throws IOException{  
-        //创建一个Excel(or new XSSFWorkbook())  
-        Workbook wb = new HSSFWorkbook();  
-        //创建表格  
-        Sheet sheet = wb.createSheet("人口样本");  
-        //创建行  
-        Row row = sheet.createRow(0);  
-        //设置行高  
-        row.setHeightInPoints(30); 
-        Cell cell = row.createCell(0);
-        for(int i=0;i<=13;i++)
-        {
-        	switch(i){
-        	case 0:
-        		row.createCell(0).setCellValue("price");
-        		break;
-        	case 1:
-        		row.createCell(1).setCellValue("area");
-        		break;
-        	case 2:
-        		row.createCell(2).setCellValue("unit_price");
-        		break;
-        	case 3:
-        		row.createCell(3).setCellValue("fitment");
-        		break;
-        	case 4:
-        		row.createCell(4).setCellValue("volume_rate");
-        		break;
-        	case 5:
-        		row.createCell(5).setCellValue("green_rate");
-        		break;
-        	case 6:
-        		row.createCell(6).setCellValue("rooms");
-        		break;
-        	case 7:
-        		row.createCell(7).setCellValue("halls");
-        		break;
-        	case 8:
-        		row.createCell(8).setCellValue("storeys");
-        		break;
-        	case 9:
-        		row.createCell(9).setCellValue("direction");
-        		break;
-        	}
-        }
-    //标题设置完毕,下面开始填充数据
-        Vector<String> rds = FileTool.Load(Folder,"UTF-8");
-        for(int i=0;i<rds.size();i++)
-        {
-        	String element=rds.elementAt(i);
-			JSONObject obj=JSONObject.fromObject(element);
-        	row = sheet.createRow(i+1); 
-        	for(int k=0;k<=13;k++)
-        	{
-        		switch(k){
-        		case 0:
-            		row.createCell(0).setCellValue(obj.getDouble(""));
-            		break;
-            	case 1:
-            		row.createCell(1).setCellValue(obj.getDouble(""));
-            		break;
-            	case 2:
-            		row.createCell(2).setCellValue(obj.getDouble(""));
-            		break;
-            	case 3:
-            		row.createCell(3).setCellValue(obj.getDouble(""));
-            		break;
-            	case 4:
-            		row.createCell(4).setCellValue(obj.getDouble(""));
-            		break;
-            	}
-        		
-        	}
-        	
-            
-        }   
-        FileOutputStream fos = new FileOutputStream(Folder.replace(".txt", "")+"_excel.csv");  
-        wb.write(fos);  
-        if(null != fos){  
-            fos.close();  
-        }  
-    }
+	public  double price;
+	public  double area;
+	public  double unit_price;
+	public  int fitment;
+	public  double volume_rate;
+	public  double green_rate;
+	public  int rooms;
+	public  int halls;
+	public  int kitchen;
+	public  int bathrooms;
+	public  double  floor;
+	public  double direction;
+	public static String Folder = "D:\\test\\woaiwojia_rentout_2016_0401.txt";
+
+	public static void main(String argv[]) throws Exception {
+		System.out.println("测试开始!");
+		writeExcel();
+		System.out.println("测试结束!");
+
+	}
+
+	public static void writeExcel() throws IOException {
+
+		Workbook wb = new HSSFWorkbook();
+		Sheet sheet = designExcel();
+		Vector<String> rds = FileTool.Load(Folder, "UTF-8");
+
+		JSONObject obj;
+		String element;
+		Row row;
+		for (int i = 0; i < rds.size(); i++) {
+			element = rds.elementAt(i);
+			//System.out.println(element);
+			obj = JSONObject.fromObject(element);
+			Txt_Excel te=new Txt_Excel(obj);
+			row = sheet.createRow(i + 1);
+			for (int k = 0; k <= 11; k++) {
+				switch (k) {
+					case 0:
+						row.createCell(0).setCellValue(te.price);
+						break;
+					case 1:
+						row.createCell(1).setCellValue(te.area);
+						break;
+					case 2:
+						row.createCell(2).setCellValue(te.unit_price);
+						break;
+					case 3:
+						row.createCell(3).setCellValue(te.fitment);
+						break;
+					case 6:
+						row.createCell(6).setCellValue(te.rooms);
+						break;
+					case 7:
+						row.createCell(7).setCellValue(te.halls);
+						break;
+					case 8:
+						row.createCell(8).setCellValue(te.kitchen);
+						break;
+					case 9:
+						row.createCell(9).setCellValue(te.bathrooms);
+						break;
+					case 10:
+						row.createCell(10).setCellValue(te.floor);
+						break;
+					case 11:
+						row.createCell(11).setCellValue(te.direction);
+						break;
+				}
+
+			}
 
 
+		}
+		FileOutputStream fos = new FileOutputStream(Folder.replace(".txt", "") + "_excel.csv");
+		wb.write(fos);
+		if (null != fos) {
+			fos.close();
+		}
+	}
+
+	public static Sheet designExcel() {
+		//创建一个Excel(or new XSSFWorkbook())
+		Workbook wb = new HSSFWorkbook();
+		//创建表格
+		Sheet sheet = wb.createSheet("出租房价");
+		//创建行
+		Row row = sheet.createRow(0);
+		//设置行高
+		row.setHeightInPoints(30);
+		Cell cell = row.createCell(0);
+		for (int i = 0; i <= 11; i++) {
+			switch (i) {
+				case 0:
+					row.createCell(0).setCellValue("price");
+					break;
+				case 1:
+					row.createCell(1).setCellValue("area");
+					break;
+				case 2:
+					row.createCell(2).setCellValue("unit_price");
+					break;
+				case 3:
+					row.createCell(3).setCellValue("fitment");
+					break;
+				case 6:
+					row.createCell(6).setCellValue("rooms");
+					break;
+				case 7:
+					row.createCell(7).setCellValue("halls");
+					break;
+				case 8:
+					row.createCell(8).setCellValue("kitchen");
+					break;
+				case 9:
+					row.createCell(9).setCellValue("bathroom");
+					break;
+				case 10:
+					row.createCell(10).setCellValue("floor");
+					break;
+				case 11:
+					row.createCell(11).setCellValue("direction");
+					break;
+			}
+		}
+		return sheet;
+	}
+
+	Txt_Excel(JSONObject obj)throws IOException{
+		try{
+
+			price=obj.getDouble("price");
+			area=obj.getDouble("area");
+			unit_price=obj.getDouble("unit_price");
+
+			if(obj.containsKey("fitment")){
+				fitment=setFitment(obj.getString("fitment"));
+			}else{
+				fitment=0;
+			}
+
+			if(obj.containsKey("volume_rate")){
+				if(!obj.getString("volume_rate").equals("null")){
+					volume_rate=obj.getDouble("volume_rate");
+				}else {
+					volume_rate=0;
+				}
+
+			}else{
+				volume_rate=0;
+			}
+
+			if(obj.containsKey("green_rate")){
+				if(!obj.getString("green_rate").equals("null")){
+					green_rate=obj.getDouble("green_rate");
+				}else {
+					green_rate=0;
+				}
+
+			}else {
+				green_rate=0;
+			}
+
+			//"layout":{"rooms":"2","halls":"1","kitchen":"","bathrooms":"1"},
+			JSONObject layout=obj.getJSONObject("layout");
+			if(layout.getString("rooms").length()!=0){
+				rooms=layout.getInt("rooms");
+			}else {
+				rooms=0;
+			}
+			if(layout.getString("halls").length()!=0){
+				halls=layout.getInt("halls");
+			}else {
+				halls=0;
+			}
+			if(layout.getString("kitchen").length()!=0){
+				kitchen=layout.getInt("kitchen");
+			}else {
+				kitchen=0;
+			}
+			if(layout.getString("bathrooms").length()!=0){
+				bathrooms=layout.getInt("bathrooms");
+			}else {
+				bathrooms=0;
+			}
+
+			//"storeys":{"flooron":"下部","floors":"21"}
+			JSONObject storeys=obj.getJSONObject("storeys");
+			String flooron=storeys.getString("flooron");
+			int floors=storeys.getInt("floors");
+			floor=setFloor(flooron,floors);
+
+			direction=setDirection(obj.getString("direction"));
+			if(obj.containsKey("direction")){
+				direction=setDirection(obj.getString("direction"));
+			}else {
+				direction=0;
+			}
+
+		}catch (JSONException e){
+
+			System.out.println(e.getMessage());
+			//System.out.println(obj);
+		}
+
+	}
+	public static double setDirection(String direction){
+		double value=0;
+
+		switch (direction){
+			case "南北": value=2;
+				break;
+			case "南": value=1.5;
+				break;
+			case "东南": value=1;
+				break;
+			case "西南": value=0.5;
+				break;
+			case "东西": value=0.1;
+				break;
+			case "东": value=-0.5;
+				break;
+			case "西": value=-1;
+				break;
+			case "东北": value=-1.5;
+				break;
+			case "西北": value=-2;
+				break;
+			case "北": value=-2.5;
+				break;
+		}
+		return value;
+	}
+	public static int setFitment(String fitment){
+		int value=0;
+
+		switch (fitment){
+			case "豪华": value=5;
+				break;
+			case "精装": value=4;
+				break;
+			case "简装": value=3;
+				break;
+			case "普装": value=2;
+				break;
+			case "毛坯": value=1;
+				break;
+	}
+		return value;
+	}
+	public static double setFloor(String flooron,int floors){
+		double value=0;
+		double ratio=0;
+		switch (flooron){
+			case "上部":ratio=1;
+				break;
+			case "中部":ratio=2;
+				break;
+			case "下部":ratio=3;
+				break;
+		}
+
+		//6层：3-4；12层：8-10；18层：13-15；33层：26-28
+		if(floors<=6){
+			if(ratio==1){
+				value=102;
+			}else if(ratio==2){
+				value=109;
+			}else{
+				value=87;
+			}
+		}else if(floors>6&&floors<=12){
+			if(ratio==1){
+				value=96;
+			}else if(ratio==2){
+				value=103;
+			}else{
+				value=105;
+			}
+		}else if(floors>12&&floors<18){
+			if(ratio==1){
+				value=94;
+			}else if(ratio==2){
+				value=103;
+			}else{
+				value=105;
+			}
+		}else if(floors>18){
+			if(ratio==1){
+				value=96;
+			}else if(ratio==2){
+				value=104;
+			}else{
+				value=102;
+			}
+		}
+		return value;
+	}
 }
---
+++ src/utils/geotext/test.java
@@ -0,0 +1,326 @@
+package utils.geotext;
+
+
+        import java.io.BufferedWriter;
+        import java.io.File;
+        import java.io.FileOutputStream;
+        import java.io.IOException;
+        import java.io.OutputStreamWriter;
+        import java.util.Vector;
+
+        import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+        import org.apache.poi.ss.usermodel.Cell;
+        import org.apache.poi.ss.usermodel.Row;
+        import org.apache.poi.ss.usermodel.Sheet;
+        import org.apache.poi.ss.usermodel.Workbook;
+        import utils.FileTool;
+
+public class test {
+    public String TITLE;
+    public double lng = 0.0;
+    public double lat = 0.0;
+    public String DIRECTION = null;
+    public static Object INVALID_DEGREE;
+    public String TIME;
+    public String PRICE;
+    public String HOUSE_TYPE;
+    public String COMMUNITY;
+    public String ADDRESS;
+    public String PARTMENT;
+    public String AREA;
+    public String FLOOR;
+    public String DECORATION;
+    public String TRAFFIC;
+    public String URL;
+    public String date;
+    public String COORDINATE;
+    public String DEPOSIT;
+    public String RENT_TYPE;
+    public String PROPERTY_TYPE;
+    public String LOCATION;
+    public String SOURCE;
+    public String DOWN_PAYMENT;
+    public String UNIT_PRICE;
+    public String ORIENTATION;
+    public String FITMENT;
+    public String GENERAL_SITUATION;
+    public String infloor;
+    public String totalfloor;
+    public int roughcast;
+    public int Simple_decoration;
+    public int Fine_decoration;
+    public int Luxury_decoration;
+    public int Moderate_decoration;
+    public int south;
+    public int north;
+    public int east;
+    public int west;
+    public int south_north;
+    public int east_west;
+    public int west_south;
+    public int east_north;
+    public int west_north;
+    public int east_south;
+    public String DEVELOPER;
+    public String PROPERTY;
+    public String PROPERTY_FEE;
+    public String TOTAL_AREA;
+    public String BUILT_YEAR;
+    public String HOUSEHOLDS;
+    public String VOLUME_RATE;
+    public String PARK;
+    public String GREEN_RATE;
+    public Double LNG;
+    public Double LAT;
+    public String Name;
+    public String PostCoor;
+    public String PostCoorLN;
+    public String PostCoorLA;
+    public String PostReg;
+    public String Code;
+    public String CodeAddr;
+    public String CodeCoor;
+    public String CodeCoorLN;
+    public String CodeCoorLA;
+    public String CodeReg;
+    public String CtfId;
+    public String Home;
+    public String Gender;
+    public String Birth;
+    public String PostAddr;
+    public String Mobile;
+    public static String Folder="D:/zhouxiang/人口数据/宾馆数据/人口统计/人口样本.txt";
+    public static void main(String argv[]) throws Exception{
+        System.out.println("测试开始!");
+        writeExcel();
+        System.out.println("测试结束!");
+
+    }
+    public static void writeExcel() throws IOException{
+        //创建一个Excel(or new XSSFWorkbook())
+        Workbook wb = new HSSFWorkbook();
+        //创建表格
+        Sheet sheet = wb.createSheet("人口样本");
+        //创建行
+        Row row = sheet.createRow(0);
+        //设置行高
+        row.setHeightInPoints(30);
+        Cell cell = row.createCell(0);
+        for(int i=0;i<=13;i++)
+        {
+            switch(i){
+                case 0:
+                    row.createCell(0).setCellValue("Name");
+                    break;
+                case 1:
+                    row.createCell(1).setCellValue("PostCoorLN");
+                    break;
+                case 2:
+                    row.createCell(2).setCellValue("PostCoorLA");
+                    break;
+                case 3:
+                    row.createCell(3).setCellValue("PostReg");
+                    break;
+                case 4:
+                    row.createCell(4).setCellValue("Code");
+                    break;
+                case 5:
+                    row.createCell(5).setCellValue("CodeCoorLN");
+                    break;
+                case 6:
+                    row.createCell(6).setCellValue("CodeCoorLA");
+                    break;
+                case 7:
+                    row.createCell(7).setCellValue("CodeReg");
+                    break;
+                case 8:
+                    row.createCell(8).setCellValue("CtfId");
+                    break;
+                case 9:
+                    row.createCell(9).setCellValue("Home");
+                    break;
+                case 10:
+                    row.createCell(10).setCellValue("Gender");
+                    break;
+                case 11:
+                    row.createCell(11).setCellValue("Birth");
+                    break;
+                case 12:
+                    row.createCell(12).setCellValue("PostAddr");
+                    break;
+                case 13:
+                    row.createCell(13).setCellValue("Mobile");
+                    break;
+            }
+        }
+        //标题设置完毕,下面开始填充数据
+        Vector<String> rds = FileTool.Load(Folder,"UTF-8");
+        for(int i=0;i<rds.size();i++)
+        {
+            String element=rds.elementAt(i);
+            test poi = new test(element);
+            row = sheet.createRow(i+1);
+            for(int k=0;k<=13;k++)
+            {
+                switch(k){
+                    case 0:
+                        row.createCell(0).setCellValue(poi.ADDRESS);
+                        break;
+                    case 1:
+                        row.createCell(1).setCellValue(poi.PostCoorLN);
+                        break;
+                    case 2:
+                        row.createCell(2).setCellValue(poi.PostCoorLA);
+                        break;
+                    case 3:
+                        row.createCell(3).setCellValue(poi.PostReg);
+                        break;
+                    case 4:
+                        row.createCell(4).setCellValue(poi.Code);
+                        break;
+                    case 5:
+                        row.createCell(5).setCellValue(poi.CodeCoorLN);
+                        break;
+                    case 6:
+                        row.createCell(6).setCellValue(poi.CodeCoorLA);
+                        break;
+                    case 7:
+                        row.createCell(7).setCellValue(poi.CodeReg);
+                        break;
+                    case 8:
+                        row.createCell(8).setCellValue(poi.CtfId);
+                        break;
+                    case 9:
+                        row.createCell(9).setCellValue(poi.Home);
+                        break;
+                    case 10:
+                        row.createCell(10).setCellValue(poi.Gender);
+                        break;
+                    case 11:
+                        row.createCell(11).setCellValue(poi.Birth);
+                        break;
+                    case 12:
+                        row.createCell(12).setCellValue(poi.PostAddr);
+                        break;
+                    case 13:
+                        row.createCell(13).setCellValue(poi.Mobile);
+                        break;
+                }
+
+            }
+
+
+        }
+        FileOutputStream fos = new FileOutputStream(Folder.replace(".txt", "")+"_excel.csv");
+        wb.write(fos);
+        if(null != fos){
+            fos.close();
+        }
+    }
+    test(String line) throws IOException
+    {
+        try{
+            if(line.indexOf("<Name>")!=-1)
+                Name=getStrByKey(line,"<Name>","</Name>");
+            if(line.indexOf("<PostCoor>")!=-1){
+                PostCoor=getStrByKey(line,"<PostCoor>","</PostCoor>").replace("&nbsp;", "").replace("&amp;","").replace("nb;","").replace("nbsp","").replace("|","");
+                String[] arry=PostCoor.split(";");
+                PostCoorLN=arry[0];
+                PostCoorLA=arry[1];
+
+            }
+            if(line.indexOf("<PostReg>")!=-1)
+                PostReg=getStrByKey(line,"<PostReg>","</PostReg>").replace("&nbsp;", "").replace("&amp;","").replace("nb;","").replace("nbsp","").replace("|","").replace(";","");
+            if(line.indexOf("<Code>")!=-1)
+                Code= getStrByKey(line,"<Code>","</Code>").replace("[面议]","").replace("[押一付三]","").replace("元/月","").replace("&nbsp;", "").replace("&amp;","").replace("nb;","").replace("nbsp","").replace("|","").replace(";","");
+            if(line.indexOf("<CodeCoor>")!=-1){
+                CodeCoor=getStrByKey(line,"<CodeCoor>","</CodeCoor>").replace("&nbsp;", "").replace("&amp;","").replace("nb;","").replace("nbsp","").replace("|","");
+                String[] arry=CodeCoor.split(";");
+                CodeCoorLN=arry[0];
+                CodeCoorLA=arry[1];
+            }
+            if(line.indexOf("<CodeReg>")!=-1)
+                CodeReg=getStrByKey(line,"<CodeReg>","</CodeReg>").replace("&nbsp;", "").replace("&amp;","").replace("nb;","").replace("nbsp","").replace("|","").replace(";","");
+            if(line.indexOf("<CtfId>")!=-1)
+                CtfId=getStrByKey(line,"<CtfId>","</CtfId>").replace("(地图)","").replace("&nbsp;", "").replace("&amp;","").replace("nb;","").replace("nbsp","").replace("|","").replace(";","");
+            if(line.indexOf("<Home>")!=-1)
+                Home=getStrByKey(line,"<Home>","</Home>").replace("(地图)","").replace("&nbsp;", "").replace("&amp;","").replace("nb;","").replace("nbsp","").replace("|","").replace(";","");
+            if(line.indexOf("<Gender>")!=-1)
+                Gender=getStrByKey(line,"<Gender>","</Gender>").replace("(地图)","").replace("&nbsp;", "").replace("&amp;","").replace("nb;","").replace("nbsp","").replace("|","").replace(";","");
+            if(line.indexOf("<Birth>")!=-1)
+                Birth=getStrByKey(line,"<Birth>","</Birth>").replace("(地图)","").replace("&nbsp;", "").replace("&amp;","").replace("nb;","").replace("nbsp","").replace("|","").replace(";","");
+            if(line.indexOf("<PostAddr>")!=-1)
+                PostAddr=getStrByKey(line,"<PostAddr>","</PostAddr>").replace("(小型小区)","").replace("(中型小区)","").replace("(大型小区)","").replace("&nbsp;", "").replace("&amp;","").replace("nb;","").replace("nbsp","").replace("|","").replace(";","");
+            if(line.indexOf("<Mobile>")!=-1)
+                Mobile=getStrByKey(line,"<Mobile>","</Mobile>").replace("户","").replace("&nbsp;", "").replace("&amp;","").replace("nb;","").replace("nbsp","").replace("|","").replace(";","");
+        }catch (NullPointerException e) {
+
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+            write_append(line,Folder.replace(".txt", "")+"_exception.txt");
+        }
+
+    }
+
+    //提取每个标签里的内容
+    public static String getStrByKey(String sContent, String sStart, String sEnd) {
+        String sOut ="";
+        int fromIndex = 0;
+        int iBegin = 0;
+        int iEnd = 0;
+        int iStart=sContent.indexOf("</Version>");
+        String temp=" ";
+        if (iStart < 0) {
+            return null;
+        }
+        for (int i = 0; i < iStart; i++) {
+            // 找出某位置，并找出该位置后的最近的一个匹配
+            iBegin = sContent.indexOf(sStart, fromIndex);
+            if (iBegin >= 0)
+            {
+                iEnd = sContent.indexOf(sEnd, iBegin + sStart.length());
+                if (iEnd <= iBegin)
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                return sOut;
+            }
+            if (iEnd > 0&&iEnd!=iBegin + sStart.length())
+            {
+                sOut += sContent.substring(iBegin + sStart.length(), iEnd);
+            }
+            else
+                return temp;
+            if (iEnd > 0)
+            {
+                fromIndex = iEnd + sEnd.length();
+            }
+        }
+        return sOut;
+    }
+    public static void write_append(String line,String pathname)  throws IOException
+    {
+        try
+        {
+
+            File writefile=new File(pathname);
+            if(!writefile.exists())
+            {
+                writefile.createNewFile();
+            }
+            OutputStreamWriter write = new OutputStreamWriter(new FileOutputStream(writefile,true),"UTF-8");
+            BufferedWriter writer = new BufferedWriter(write);
+            writer.write(line);
+            writer.write("\r\n");
+            writer.close();
+        }catch(Exception e) {
+            e.printStackTrace();
+
+        }
+    }
+
+
+}
